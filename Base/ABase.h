69e065edfa5571454539edc6885dbf3c450e0a58
Prof1983 <prof1983@yandex.ru>
2012-06-15 17:32:56 +0600

    Updated for Qt (GCC) and for Doxygen doc generation

--- Base/ABase.h
+++ Base/ABase.h
@@ -1,27 +1,29 @@
-﻿/* Base consts and types
-Author(Prof1983 prof1983@ya.ru)
-Created(06.03.2008)
-LastMod(14.06.2012)
-Version(0.5)
-*/
+﻿/*  Base consts and types
+ *  Author(Prof1983 prof1983@ya.ru)
+ *  Created(06.03.2008)
+ *  LastMod(15.06.2012)
+ *  Version(0.5)
+ */
 
 #ifndef ABaseH
 #define ABaseH
+
+#include <stdint.h>
 
 /* --- Types0 --- */
 
 typedef float AFloat32;
 typedef double AFloat64;
 typedef int AInt;
-typedef __int8 AInt8;
-typedef __int16 AInt16;
-typedef __int32 AInt32;
-typedef __int64 AInt64;
+typedef int8_t AInt8; //typedef __int8 AInt8;
+typedef int16_t AInt16; //typedef __int16 AInt16;
+typedef int32_t AInt32; //typedef __int32 AInt32;
+typedef int64_t AInt64; //typedef __int64 AInt64;
 typedef unsigned int AUInt;
-typedef unsigned __int8 AUInt8;
-typedef unsigned __int16 AUInt16;
-typedef unsigned __int32 AUInt32;
-typedef unsigned __int64 AUInt64;
+typedef uint8_t AUInt8; //typedef unsigned __int8 AUInt8;
+typedef uint16_t AUInt16; //typedef unsigned __int16 AUInt16;
+typedef uint32_t AUInt32; //typedef unsigned __int32 AUInt32;
+typedef uint64_t AUInt64; //typedef unsigned __int64 AUInt64;
 
 /* --- Types1 --- */
 
@@ -31,23 +33,29 @@ typedef int ABoolean;
 typedef AFloat64 AFloat;
 typedef AInt AInteger;
 
-const AFalse = 0;
-const ATrue = 1;
+#define AFalse 0 //const AFalse = 0;
+#define ATrue 1 //const ATrue = 1;
 
 /* --- Types2 --- */
 
-// Версия. Имеет формат $AABBCCDD = AA.BB.CC.DD
+/** Version.
+ *  Format $AABBCCDD = AA.BB.CC.DD
+ *  AA - Major
+ *  BB - Minor
+ *  CC - Revision/Patch
+ *  DD - Build
+ */
 typedef AUInt32 AVersion;
-// Маска для проверки соответсвия версий модулей
+
+/** Маска для проверки соответсвия версий модулей */
 #define AVersionMask 0xFFFF0000;
 //const AVersion            AVersionMask = 0xFFFF0000;
 
-/*
-	Возвращаемое значение функций. Значения:
-	<0 - ошибка при выполнении,
-	0 - выполнение прошло успешно
-	>0 - выполнение прошло успешно, но есть замечания или информация
-*/
+/** Возвращаемое значение функций. Значения:
+ *  <0 - ошибка при выполнении,
+ *  0 - выполнение прошло успешно
+ *  >0 - выполнение прошло успешно, но есть замечания или информация
+ */
 typedef AInt AError;
 
 typedef AInt AConfig;

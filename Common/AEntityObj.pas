7d758c72856991bde2d530a6173f228de67d836f
Prof1983 <prof1983@yandex.ru>
2012-06-21 09:20:44 +0600

    Updated AEntityObj.pas

--- Common/AEntityObj.pas
+++ Common/AEntityObj.pas
@@ -2,7 +2,7 @@
 @Abstract(Сущность)
 @Author(Prof1983 prof1983@ya.ru)
 @Created(11.05.2007)
-@LastMod(08.06.2012)
+@LastMod(21.06.2012)
 @Version(0.5)
 }
 unit AEntityObj;
@@ -25,21 +25,6 @@ type
     property EntityType: TAId read FEntityType;
   end;
   //TAEntity = TAEntityObject;
-
-// Базовые типы сущностей [0..1024]
-
-const
-  AINone           = 0; // нет записи
-  AIType           = 1; // тип
-const
-  AIEmptyType      = 2; // Пусто
-  AINullType       = 3; // Null
-  AIBoolType       = 4; // Boolean
-  AIIntType        = 5; // Целое
-  AIFloatType      = 6; // Значение с плавающей точкой
-  AIStringType     = 7; // строка
-  AIDateTimeType   = 8; // дата/время
-  AICollectionType = 9; // коллекция
 
 implementation
 

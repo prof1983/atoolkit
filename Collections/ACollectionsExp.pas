df1b6e8a5d526d0fe7378e5959610e13c0b56fec
Prof1983 <prof1983@yandex.ru>
2012-07-24 13:37:53 +0600

    Added ACollectionsExp.pas

---
+++ Collections/ACollectionsExp.pas
@@ -0,0 +1,21 @@
+{**
+@Abstract ACollections
+@Author Prof1983 <prof1983@ya.ru>
+@Created 12.12.2011
+@LastMod 24.07.2012
+}
+unit ACollectionsExp;
+
+interface
+
+uses
+  ACollections;
+
+exports
+  ACollections.Init name 'ACollections_Init',
+  ACollections.Done name 'ACollections_Done';
+
+implementation
+
+end.
+ 

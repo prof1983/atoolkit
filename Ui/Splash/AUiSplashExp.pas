f2ea246a4f2088cbb28a513e0e3de06f2426d8c0
Prof1983 <prof1983@yandex.ru>
2012-07-25 09:49:27 +0600

    Updated AUiSplash

---
+++ Ui/Splash/AUiSplashExp.pas
@@ -0,0 +1,24 @@
+{**
+@Abstract AUiSplash export functions
+@Author Prof1983 <prof1983@ya.ru>
+@Created 25.07.2012
+@LastMod 25.07.2012
+}
+unit AUiSplashExp;
+
+interface
+
+uses
+  AUiSplash;
+
+exports
+  AUiSplash_Fin,
+  AUiSplash_Hide,
+  AUiSplash_Init,
+  AUiSplash_Sleep,
+  AUiSplash_StepItP;
+
+implementation
+
+end.
+ 

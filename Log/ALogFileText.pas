58510da69c3334a3c299d463dcfd1cfa409dd9a3
Prof1983 <prof1983@yandex.ru>
2012-11-12 10:01:13 +0600

    Updated ALogFileText

--- Log/ALogFileText.pas
+++ Log/ALogFileText.pas
@@ -1,8 +1,8 @@
 ﻿{**
-@Abstract(Классы для записи собщений программы в БД или файл)
-@Author(Prof1983 <prof1983@ya.ru>)
-@Created(16.08.2005)
-@LastMod(13.07.2012)
+@Abstract Классы для записи собщений программы в БД или файл
+@Author Prof1983 <prof1983@ya.ru>
+@Created 16.08.2005
+@LastMod 12.11.2012
 
 Uses
   @link ABase
@@ -19,27 +19,33 @@ uses
   ABase, ALogDocumentImpl, ALogFileTextUtils, ATypes;
 
 type //** Запись в файл
-  TProfLogFileText3 = class(TALogDocument)
+  TALogFileText = class(TALogDocument)
   private
     //** Имя файла
     FFileName: WideString;
   public
+    procedure SetFileName(const Value: WideString);
+  public
     //** Добавить сообщение
     function AddToLog(AGroup: TLogGroupMessage; AType: TLogTypeMessage; const AStrMsg: WideString): Integer; override;
     function Initialize(): AError; override;
-    class function SaveLog(Err: TLogTypeMessage; AStrMsg: string; AFileName: string = ''; AFileNameOld: string = ''; AFilePath: string = ''): Boolean;
+    {class function SaveLog(Err: TLogTypeMessage; AStrMsg: string; AFileName: string = '';
+        AFileNameOld: string = ''; AFilePath: string = ''): Boolean;}
   public
     constructor Create();
   public
     //** Имя файла
-    property FileName: WideString read FFileName write FFileName;
+    property FileName: WideString read FFileName write SetFileName;
   end;
+
+  //TLogFileText = TALogFileText;
+  //TProfLogFileText3 = TALogFileText;
 
 implementation
 
-{ TProfLogFileText }
+{ TALogFileText }
 
-function TProfLogFileText3.AddToLog(AGroup: TLogGroupMessage; AType: TLogTypeMessage; const AStrMsg: WideString): Integer;
+function TALogFileText.AddToLog(AGroup: TLogGroupMessage; AType: TLogTypeMessage; const AStrMsg: WideString): Integer;
 var
   S: string;
 begin
@@ -54,22 +60,30 @@ begin
   Result := 1;
 end;
 
-constructor TProfLogFileText3.Create();
+constructor TALogFileText.Create();
 begin
   inherited Create(lFile);
 end;
 
-function TProfLogFileText3.Initialize(): AError;
+function TALogFileText.Initialize(): AError;
 begin
   Result := inherited Initialize();
   // Проверка сущестрования директории
   ForceDirectories(ExtractFilePath(FFileName));
 end;
 
-class function TProfLogFileText3.SaveLog(Err: TLogTypeMessage; AStrMsg: string; AFileName: string = ''; AFileNameOld: string = ''; AFilePath: string = ''): Boolean;
+procedure TALogFileText.SetFileName(const Value: WideString);
+begin
+  FFileName := Value;
+  // Проверка сущестрования директории
+  ForceDirectories(ExtractFilePath(FFileName));
+end;
+
+{class function TALogFileText.SaveLog(Err: TLogTypeMessage; AStrMsg: string; AFileName: string = '';
+    AFileNameOld: string = ''; AFilePath: string = ''): Boolean;
 begin
   Result := ALogFileTextUtils.SaveLog(Err, AStrMsg, AFileName, AFileNameOld, AFilePath);
-end;
+end;}
 
 end.
 

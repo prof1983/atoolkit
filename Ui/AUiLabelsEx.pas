7d1a0d9fb3ae177f35c925b7541c88f9af44f3ce
Prof1983 <prof1983@yandex.ru>
2012-11-14 12:12:22 +0600

    Updated AUiLabels

---
+++ Ui/AUiLabelsEx.pas
@@ -0,0 +1,33 @@
+{**
+@Abstract AUiLabel extended functions
+@Author Prof1983 <prof1983@ya.ru>
+@Created 14.11.2012
+@LastMod 14.11.2012
+}
+unit AUiLabelsEx;
+
+{$define AStdCall}
+
+interface
+
+uses
+  ABase,
+  AUiBase, AUiLabels, AUiTextView;
+
+{** Создает новый элемент текстового вывода
+    @param Typ - 0-Label; 1-Memo }
+function AUiLabel_New2(Parent: AControl; Typ: AInt): AControl; {$ifdef AStdCall}stdcall;{$endif}
+
+implementation
+
+function AUiLabel_New2(Parent: AControl; Typ: AInt): AControl;
+begin
+  if (Typ = 0) then
+    Result := AUiLabel_New(Parent)
+  else if (Typ = 1) then
+    AUiTextView_New(Parent, 0)
+  else
+    Result := 0;
+end;
+
+end.

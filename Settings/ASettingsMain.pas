7e74b0dbdf9eeacfce36a5ca8e2e6ad35f900932
Prof1983 <prof1983@yandex.ru>
2012-11-21 11:10:20 +0600

    Updated ASettingsMain

--- Settings/ASettingsMain.pas
+++ Settings/ASettingsMain.pas
@@ -2,7 +2,7 @@
 @Abstract ASettings main functions
 @Author Prof1983 <prof1983@ya.ru>
 @Created 13.08.2012
-@LastMod 14.08.2012
+@LastMod 21.11.2012
 }
 unit ASettingsMain;
 
@@ -14,13 +14,31 @@ uses
 
 // --- ASettings ---
 
+function ASettings_Close(Config: AConfig): AError; stdcall;
+
 function ASettings_ReadBoolDef(Config: AConfig; const Section, Name: AString_Type; DefValue: ABoolean): ABoolean; stdcall;
 
 function ASettings_ReadBoolDefP(Config: AConfig; const Section, Name: APascalString; DefValue: ABoolean): ABoolean; stdcall;
 
+function ASettings_ReadIntegerDefP(Config: AConfig; const Section, Name: APascalString; DefValue: AInteger): AInteger; stdcall;
+
+function ASettings_ReadStringDefP(Config: AConfig; const Section, Name, DefValue: APascalString): APascalString; stdcall;
+
+function ASettings_WriteIntegerP(Config: AConfig; const Section, Name: APascalString; Value: AInteger): AError; stdcall;
+
 implementation
 
 // --- ASettings ---
+
+function ASettings_Close(Config: AConfig): AError;
+begin
+  try
+    TAbstractSettings(Config).Close();
+    Result := 0;
+  except
+    Result := -1;
+  end;
+end;
 
 function ASettings_ReadBoolDef(Config: AConfig; const Section, Name: AString_Type; DefValue: ABoolean): ABoolean;
 begin
@@ -45,6 +63,47 @@ begin
     Result := TAbstractSettings(Config).ReadBool(Section, Name, DefValue)
   except
     Result := DefValue;
+  end;
+end;
+
+function ASettings_ReadIntegerDefP(Config: AConfig; const Section, Name: APascalString; DefValue: AInteger): AInteger;
+begin
+  if (Config <> 0) then
+  begin
+    Result := DefValue;
+    Exit;
+  end;
+
+  try
+    Result := TAbstractSettings(Config).ReadInteger(Section, Name, DefValue);
+  except
+    Result := DefValue;
+  end;
+end;
+
+function ASettings_ReadStringDefP(Config: AConfig; const Section, Name, DefValue: APascalString): APascalString;
+begin
+  if (Config = 0) then
+    Result := ''
+  else
+    TAbstractSettings(Config).ReadString(Section, Name, DefValue, Result);
+end;
+
+function ASettings_WriteIntegerP(Config: AConfig; const Section, Name: APascalString; Value: AInteger): AError;
+begin
+  if (Config = 0) then
+  begin
+    Result := -1;
+    Exit;
+  end;
+
+  try
+    if TAbstractSettings(Config).WriteInteger(Section, Name, Value) then
+      Result := 0
+    else
+      Result := -1;
+  except
+    Result := -1;
   end;
 end;
 

60b29e4fe72334496c3bb5ff29127be7a233a917
Prof1983 <prof1983@yandex.ru>
2012-11-22 14:37:25 +0600

    Added AUiProgressBar

---
+++ Ui/AUiProgressBar.pas
@@ -0,0 +1,69 @@
+{**
+Author Prof1983 <prof1983@ya.ru>
+Created 22.11.2012
+LastMod 22.11.2012
+}
+unit AUiProgressBar;
+
+{$define AStdCall}
+
+interface
+
+uses
+  ComCtrls, Controls,
+  ABase, AUiBase, AUiData;
+
+// --- AUiProgressBar ---
+
+function AUiProgressBar_New(Parent: AControl; Max: AInteger): AControl; {$ifdef AStdCall}stdcall;{$endif}
+
+function AUiProgressBar_StepIt(ProgressBar: AControl): AInteger; {$ifdef AStdCall}stdcall;{$endif}
+
+// --- UI_ProgressBar ---
+
+function UI_ProgressBar_New(Parent: AControl; Max: AInteger): AControl; deprecated; // Use AUiProgressBar_New()
+
+function UI_ProgressBar_StepIt(ProgressBar: AControl): AInteger; deprecated; // Use AUiProgressBar_StepIt()
+
+implementation
+
+// --- AUiProgressBar ---
+
+function AUiProgressBar_New(Parent: AControl; Max: AInteger): AControl;
+var
+  ProgressBar: TProgressBar;
+begin
+  try
+    ProgressBar := TProgressBar.Create(TWinControl(Parent));
+    ProgressBar.Parent := TWinControl(Parent);
+    ProgressBar.Max := Max;
+    Result := AddObject(ProgressBar);
+  except
+    Result := 0;
+  end;
+end;
+
+function AUiProgressBar_StepIt(ProgressBar: AControl): AInteger;
+begin
+  try
+    TProgressBar(ProgressBar).StepIt;
+    Result := TProgressBar(ProgressBar).Position;
+  except
+    Result := 0;
+  end;
+end;
+
+{ ProgressBar }
+
+function UI_ProgressBar_New(Parent: AControl; Max: AInteger): AControl;
+begin
+  Result := AuiProgressBar_New(Parent, Max);
+end;
+
+function UI_ProgressBar_StepIt(ProgressBar: AControl): AInteger;
+begin
+  Result := AUiProgressBar_StepIt(ProgressBar);
+end;
+
+end.
+ 

3d6bfb71dc07f83e9d8bb9353bd6376fbcc0a91b
Prof1983 <prof1983@yandex.ru>
2012-12-25 16:12:29 +0600

    Added AString_ToLowerP(), ASystem_GetDirectoryPathP()

--- Common/AStringUtils.pas
+++ Common/AStringUtils.pas
@@ -2,7 +2,7 @@
 @Abstract AString utils
 @Author Prof1983 <prof1983@ya.ru>
 @Created 01.08.2012
-@LastMod 06.08.2012
+@LastMod 25.12.2012
 }
 unit AStringUtils;
 
@@ -12,6 +12,10 @@ uses
   SysUtils,
   ABase, AStrings;
 
+// --- AString ---
+
+function AString_ToLowerP(const S: APascalString): APascalString;
+
 function AString_ToUpper(const S: AString_Type; out Res: AString_Type): AInteger; stdcall;
 
 function AString_ToUpperP(const S: APascalString): APascalString; stdcall;
@@ -19,6 +23,15 @@ function AString_ToUpperP(const S: APascalString): APascalString; stdcall;
 function AString_ToUpperWS(const S: AWideString): AWideString; stdcall;
 
 implementation
+
+function AString_ToLowerP(const S: APascalString): APascalString;
+begin
+  try
+    Result := SysUtils.LowerCase(S);
+  except
+    Result := '';
+  end;
+end;
 
 function AString_ToUpper(const S: AString_Type; out Res: AString_Type): AInteger;
 begin

e8ba0c92acb2d0b76b60a28abbfc766ab38f9376
Prof1983 <prof1983@yandex.ru>
2013-01-09 13:40:46 +0600

    Added AUiCheckBox_GetChecked(), AUiCheckBox_NewEx(), AUiCheckBox_SetChecked()

--- Ui/AUiCheckBox.pas
+++ Ui/AUiCheckBox.pas
@@ -10,32 +10,161 @@ unit AUiCheckBox;
 interface
 
 uses
+  Classes,
   Controls,
   StdCtrls,
   ABase,
   AUiBase,
+  AUiControls,
   AUiData;
+
+function AUiCheckBox_GetChecked(CheckBox: AControl): ABool; {$ifdef AStdCall}stdcall;{$endif}
 
 function AUiCheckBox_New(Parent: AControl): AControl; {$ifdef AStdCall}stdcall;{$endif}
 
+function AUiCheckBox_NewEx(Parent: AControl; OnChange: ACallbackProc; Left, Top,
+    Width: AInt): AControl; {$ifdef AStdCall}stdcall;{$endif}
+
+function AUiCheckBox_SetChecked(CheckBox: AControl; Value: ABool): AError; {$ifdef AStdCall}stdcall;{$endif}
+
 implementation
+
+type
+  TAUiCheckBox = object
+  public
+    CheckBox: AControl;
+    OnChange: ACallbackProc;
+  public
+    procedure DoClick(Sender: TObject);
+    procedure DoKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+  end;
+
+var
+  FCheckBox: array of TAUiCheckBox;
+
+// --- Private ---
+
+function FindCheckBox(CheckBox: AControl): Integer;
+var
+  I: Integer;
+begin
+  for I := 0 to High(FCheckBox) do
+  begin
+    if (FCheckBox[I].CheckBox = CheckBox) then
+    begin
+      Result := I;
+      Exit;
+    end;
+  end;
+  Result := -1;
+end;
+
+function _New(Parent: AControl; OnChange: ACallbackProc): AInt;
+var
+  CheckBox: TCheckBox;
+  I: Integer;
+  Res: AControl;
+begin
+  try
+    CheckBox := TCheckBox.Create(nil);
+    CheckBox.Parent := TWinControl(Parent);
+    Res := AUiData.AddObject(CheckBox);
+    I := Length(FCheckBox);
+    SetLength(FCheckBox, I + 1);
+    FCheckBox[I].CheckBox := Res;
+    FCheckBox[I].OnChange := OnChange;
+    Result := I;
+  except
+    Result := -1;
+  end;
+end;
+
+// --- AUiCheckBox ---
+
+function AUiCheckBox_GetChecked(CheckBox: AControl): ABool;
+begin
+  if not(TObject(CheckBox) is TCheckBox) then
+  begin
+    Result := False;
+    Exit;
+  end;
+  try
+    Result := TCheckBox(CheckBox).Checked;
+  except
+    Result := False;
+  end;
+end;
 
 function AUiCheckBox_New(Parent: AControl): AControl;
 var
-  CheckBox: TCheckBox;
+  I: Integer;
 begin
   if not(TObject(Parent) is TWinControl) then
   begin
     Result := 0;
     Exit;
   end;
-  try
-    CheckBox := TCheckBox.Create(nil);
-    CheckBox.Parent := TWinControl(Parent);
-    Result := AUiData.AddObject(CheckBox);
-  except
+  I := _New(Parent, nil);
+  if (I < 0) then
+  begin
     Result := 0;
+    Exit;
   end;
+  Result := FCheckBox[I].CheckBox;
+end;
+
+function AUiCheckBox_NewEx(Parent: AControl; OnChange: ACallbackProc; Left, Top,
+    Width: AInt): AControl;
+var
+  I: Integer;
+  Res: AControl;
+begin
+  if not(TObject(Parent) is TWinControl) then
+  begin
+    Result := 0;
+    Exit;
+  end;
+  I := _New(Parent, OnChange);
+  if (I < 0) then
+  begin
+    Result := 0;
+    Exit;
+  end;
+  Res := FCheckBox[I].CheckBox;
+  TCheckBox(Res).OnClick := FCheckBox[I].DoClick;
+  TCheckBox(Res).OnKeyUp := FCheckBox[I].DoKeyUp;
+  if (Res = 0) then
+  begin
+    Result := 0;
+    Exit;
+  end;
+  AUiControl_SetPosition(Res, Left, Top);
+  AUiControl_SetWidth(Res, Width);
+  Result := Res;
+end;
+
+function AUiCheckBox_SetChecked(CheckBox: AControl; Value: ABool): AError;
+begin
+  try
+    TCheckBox(CheckBox).Checked := Value;
+    Result := 0;
+  except
+    Result := -1;
+  end;
+end;
+
+{ TAUiCheckBox }
+
+procedure TAUiCheckBox.DoClick(Sender: TObject);
+begin
+  if (Assigned(Self.OnChange)) then
+    Self.OnChange(Self.CheckBox, 0);
+end;
+
+procedure TAUiCheckBox.DoKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  if (Key = 32) and (Assigned(Self.OnChange)) then
+    Self.OnChange(Self.CheckBox, 0);
 end;
 
 end.

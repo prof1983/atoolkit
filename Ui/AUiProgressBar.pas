bb1a9fa06f0572133601e111d8ee5607c40ee99d
Prof1983 <prof1983@yandex.ru>
2013-01-11 09:35:06 +0600

    Added AUiProgressBar_SetMaxPosition(), AUiProgressBar_SetPosition(), AUiProgressBar_StepBy()

--- Ui/AUiProgressBar.pas
+++ Ui/AUiProgressBar.pas
@@ -1,7 +1,7 @@
 {**
 @Author Prof1983 <prof1983@ya.ru>
 @Created 22.11.2012
-@LastMod 12.12.2012
+@LastMod 11.01.2013
 }
 unit AUiProgressBar;
 
@@ -16,6 +16,12 @@ uses
 // --- AUiProgressBar ---
 
 function AUiProgressBar_New(Parent: AControl; Max: AInteger): AControl; {$ifdef AStdCall}stdcall;{$endif}
+
+function AUiProgressBar_SetMaxPosition(ProgressBar: AControl; MaxPosition: AInt): AError; {$ifdef AStdCall}stdcall;{$endif}
+
+function AUiProgressBar_SetPosition(ProgressBar: AControl; Position: AInt): AError; {$ifdef AStdCall}stdcall;{$endif}
+
+function AUiProgressBar_StepBy(ProgressBar: AControl; Step: AInt): AInt; {$ifdef AStdCall}stdcall;{$endif}
 
 function AUiProgressBar_StepIt(ProgressBar: AControl): AInteger; {$ifdef AStdCall}stdcall;{$endif}
 
@@ -43,14 +49,54 @@ begin
   end;
 end;
 
-function AUiProgressBar_StepIt(ProgressBar: AControl): AInteger;
+function AUiProgressBar_SetMaxPosition(ProgressBar: AControl; MaxPosition: AInt): AError;
 begin
+  if not(TObject(ProgressBar) is TProgressBar) then
+  begin
+    Result := -2;
+    Exit;
+  end;
   try
-    TProgressBar(ProgressBar).StepIt;
+    TProgressBar(ProgressBar).Max := MaxPosition;
+    Result := 0;
+  except
+    Result := -1;
+  end;
+end;
+
+function AUiProgressBar_SetPosition(ProgressBar: AControl; Position: AInt): AError;
+begin
+  if not(TObject(ProgressBar) is TProgressBar) then
+  begin
+    Result := -2;
+    Exit;
+  end;
+  try
+    TProgressBar(ProgressBar).Position := Position;
+    Result := 0;
+  except
+    Result := -1;
+  end;
+end;
+
+function AUiProgressBar_StepBy(ProgressBar: AControl; Step: AInt): AInt;
+begin
+  if not(TObject(ProgressBar) is TProgressBar) then
+  begin
+    Result := -2;
+    Exit;
+  end;
+  try
+    TProgressBar(ProgressBar).Position := TProgressBar(ProgressBar).Position + Step;
     Result := TProgressBar(ProgressBar).Position;
   except
-    Result := 0;
+    Result := -1;
   end;
+end;
+
+function AUiProgressBar_StepIt(ProgressBar: AControl): AInteger;
+begin
+  Result := AUiProgressBar_StepBy(ProgressBar, 1);
 end;
 
 { ProgressBar }

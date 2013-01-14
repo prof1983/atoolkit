efc876ea837bc10f28eae8d9496e81610c071e09
Prof1983 <prof1983@yandex.ru>
2013-01-14 13:35:28 +0600

    Added AUiSpinEdit_GetValue()

--- Ui/AUiSpinEdit.pas
+++ Ui/AUiSpinEdit.pas
@@ -19,6 +19,8 @@ uses
 
 // --- AUiSpinEdit ---
 
+function AUiSpinEdit_GetValue(SpinEdit: AControl): AInt; {$ifdef AStdCall}stdcall;{$endif}
+
 function AUiSpinEdit_New(Parent: AControl): AControl; {$ifdef AStdCall}stdcall;{$endif}
 
 function AUiSpinEdit_NewEx(Parent: AControl; Value, MinValue, MaxValue: AInteger): AControl; {$ifdef AStdCall}stdcall;{$endif}
@@ -40,6 +42,28 @@ function UI_SpinEdit_NewA(Parent: AControl; Value, MinValue, MaxValue: AInteger)
 implementation
 
 // --- AUiSpinEdit ---
+
+function AUiSpinEdit_GetValue(SpinEdit: AControl): AInt;
+var
+  Obj: TObject;
+begin
+  try
+    Obj := AUiData.GetObject(SpinEdit);
+    if not(Assigned(Obj)) then
+    begin
+      Result := 0;
+      Exit;
+    end;
+    if not(Obj is TSpinEdit) then
+    begin
+      Result := 0;
+      Exit;
+    end;
+    Result := TSpinEdit(Obj).Value;
+  except
+    Result := 0;
+  end;
+end;
 
 function AUiSpinEdit_New(Parent: AControl): AControl;
 var
@@ -87,6 +111,7 @@ begin
       Exit;
     end;
     TSpinEdit(Obj).Value := Value;
+    Result := 0;
   except
     Result := -1;
   end;

5da9d5ae4eb85bc4b9d854d69748dfc479c14b3b
Prof1983 <prof1983@yandex.ru>
2013-02-20 15:00:02 +0600

    Updated AStringUtils

--- Common/AStringUtils.pas
+++ Common/AStringUtils.pas
@@ -2,9 +2,11 @@
 @Abstract AString utils
 @Author Prof1983 <prof1983@ya.ru>
 @Created 01.08.2012
-@LastMod 28.01.2013
+@LastMod 20.02.2013
 }
 unit AStringUtils;
+
+{define AStdCall}
 
 interface
 
@@ -15,26 +17,37 @@ uses
 
 // --- AString ---
 
+function AString_ToLower(const S: AString_Type; out Res: AString_Type): AInt; {$ifdef AStdCall}stdcall;{$endif}
+
 function AString_ToLowerP(const S: APascalString): APascalString;
 
-function AString_ToUpper(const S: AString_Type; out Res: AString_Type): AInteger; stdcall;
+function AString_ToUpper(const S: AString_Type; out Res: AString_Type): AInt; {$ifdef AStdCall}stdcall;{$endif}
 
-function AString_ToUpperP(const S: APascalString): APascalString; stdcall;
-
-function AString_ToUpperWS(const S: AWideString): AWideString; stdcall;
+function AString_ToUpperP(const S: APascalString): APascalString;
 
 implementation
+
+// --- AString ---
+
+function AString_ToLower(const S: AString_Type; out Res: AString_Type): AInt;
+begin
+  try
+    Result := AString_AssignP(Res, SysUtils.AnsiLowerCase(AString_ToPascalString(S)));
+  except
+    Result := 0;
+  end;
+end;
 
 function AString_ToLowerP(const S: APascalString): APascalString;
 begin
   try
-    Result := SysUtils.LowerCase(S);
+    Result := SysUtils.AnsiLowerCase(S);
   except
     Result := '';
   end;
 end;
 
-function AString_ToUpper(const S: AString_Type; out Res: AString_Type): AInteger;
+function AString_ToUpper(const S: AString_Type; out Res: AString_Type): AInt;
 begin
   try
     Result := AString_AssignP(Res, SysUtils.AnsiUpperCase(AString_ToPascalString(S)));
@@ -50,11 +63,6 @@ begin
   except
     Result := '';
   end;
-end;
-
-function AString_ToUpperWS(const S: AWideString): AWideString;
-begin
-  Result := AString_ToUpperP(S);
 end;
 
 end.

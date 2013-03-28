87784ad4dc1df2ebdf47ebc175776d304b77144a
Prof1983 <prof1983@yandex.ru>
2013-03-28 09:00:32 +0600

    Updated ACore client

--- Common/ACore.c
+++ Common/ACore.c
@@ -1,45 +1,40 @@
-﻿/*  ACore lib client
-	Author Prof1983 <prof1983@ya.ru>
-	Created 16.06.2011
-	LastMod 28.08.2012
+﻿/*
+ACore lib client
+Author Prof1983 <prof1983@ya.ru>
+Created 16.06.2011
+LastMod 27.03.2013
 */
 
+#include "ABase2.h"
 #include "ACore.h"
 #include "ALibraries.h"
 
 ALibrary Lib;
 
-int CoreLib_Open(AStr CoreLibName)
+AError
+afunc CoreLib_Open(AStr CoreLibName)
 {
-	Lib = ALibrary_OpenA(CoreLibName, 0);
-	if (Lib == NULL)
-	{
-		// тут обрабатываем ошибку, если библиотека не загрузилась
-		//ShowMessage("Plugin1.dll не загружен.");
-		return -1;
-	}
+    Lib = ALibrary_OpenA(CoreLibName, 0);
+    if (Lib == NULL)
+    {
+        return -1;
+    }
 
-	ACore_Boot = (ACore_Boot_Proc)ALibrary_GetProcAddressA(Lib, "Core_Boot");
-	ACore_Fin = (ACore_Fin_Proc)ALibrary_GetProcAddressA(Lib, "Core_Fin");
-	ACore_Init = (ACore_Init_Proc)ALibrary_GetProcAddressA(Lib, "Core_Init");
-	ACore_Run = (ACore_Run_Proc)ALibrary_GetProcAddressA(Lib, "Core_Run");
-	//ACore.Runtime = (ACore_Runtime_Proc)GetProcAddress(Lib, "Core_Runtime");
-	/*
-	Core_Boot = GetProcAddress(Lib, "Core_Boot");
-	Core_Init = GetProcAddress(Lib, "Core_Init");
-	Core_Run = GetProcAddress(Lib, "Core_Run");
-	Core_Done = GetProcAddress(Lib, "Core_Done");
-	Core_Runtime = GetProcAddress(Lib, "Core_Runtime");
-	*/
+    ACore_Boot = (ACore_Boot_Proc)ALibrary_GetProcAddressA(Lib, "ACore_Boot");
+    ACore_Fin = (ACore_Fin_Proc)ALibrary_GetProcAddressA(Lib, "ACore_Fin");
+    ACore_Init = (ACore_Init_Proc)ALibrary_GetProcAddressA(Lib, "ACore_Init");
+    ACore_Run = (ACore_Run_Proc)ALibrary_GetProcAddressA(Lib, "ACore_Run");
 
-	return 0;
+    return 0;
 }
 
-void CoreLib_Close()
+AError
+afunc CoreLib_Close()
 {
-	if (Lib != NULL)
-	{
-		ALibrary_Close(Lib);
-		Lib = NULL;
-	}
+    if (Lib != NULL)
+    {
+        ALibrary_Close(Lib);
+        Lib = NULL;
+    }
+    return 0;
 }
--- Common/ACore.h
+++ Common/ACore.h
@@ -1,14 +1,13 @@
-/*  ACore
-	Author Prof1983 <prof1983@ya.ru>
-	Created 19.08.2009
-	LastMod 28.08.2012
+/*
+Author Prof1983 <prof1983@ya.ru>
+Created 19.08.2009
+LastMod 27.03.2013
 */
 
 #ifndef ACore_H
 #define ACore_H
 
 #include "ABase2.h"
-//#include "ARuntime.h"
 
 // --- Proc types ---
 
@@ -16,7 +15,6 @@ typedef AProc ACore_Boot_Proc;
 typedef AProc ACore_Fin_Proc;
 typedef AProc ACore_Init_Proc;
 typedef AProc ACore_Run_Proc;
-//typedef APointer/*ARuntimeProcs*/ __stdcall (*A_Core_Runtime_Proc)();
 
 // --- Vars ---
 
@@ -24,25 +22,13 @@ ACore_Boot_Proc ACore_Boot;
 ACore_Fin_Proc ACore_Fin;
 ACore_Init_Proc ACore_Init;
 ACore_Run_Proc ACore_Run;
-//ACore_Runtime_Proc ACore_Runtime;
-
-/*
-struct ACoreProcs_Type
-{
-	ACore_Boot_Proc Boot;
-	ACore_Fin_Proc Fin;
-	ACore_Init_Proc Init;
-	ACore_Run_Proc Run;
-	//ACore_Runtime_Proc Runtime;
-};
-
-struct ACoreProcs_Type ACore;
-*/
 
 // ----
 
-int CoreLib_Open(AStr CoreLibName);
+AError
+afunc CoreLib_Open(AStr CoreLibName);
 
-void CoreLib_Close();
+AError
+afunc CoreLib_Close();
 
 #endif ACore_H

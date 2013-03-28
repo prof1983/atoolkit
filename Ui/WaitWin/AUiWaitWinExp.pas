0c0aa8a640e26116e8bedf067f816a56a8363d8b
Prof1983 <prof1983@yandex.ru>
2013-03-28 08:59:44 +0600

    Moved AUiWaitWin

---
+++ Ui/WaitWin/AUiWaitWinExp.pas
@@ -0,0 +1,22 @@
+{**
+@Author Prof1983 <prof1983@ya.ru>
+@Created 27.03.2013
+@LastMod 27.03.2013
+}
+unit AUiExp;
+
+interface
+
+uses
+  AUiWaitWin;
+
+exports
+  AUiWaitWin_New,
+  AUiWaitWin_SetMaxPosition,
+  AUiWaitWin_SetPosition,
+  AUiWaitWin_SetText,
+  AUiWaitWin_StepBy;
+
+implementation
+
+end.

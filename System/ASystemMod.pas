09efd148fa312b1cfb3eeb1ae9b9cb08235513fb
Prof1983 <prof1983@yandex.ru>
2013-04-04 11:35:17 +0600

    Added Mod from AModules

---
+++ System/ASystemMod.pas
@@ -0,0 +1,148 @@
+{**
+@Abstract ASystem
+@Author Prof1983 <prof1983@ya.ru>
+@Created 20.08.2007
+@LastMod 01.04.2013
+}
+unit ASystemMod;
+
+interface
+
+uses
+  ABase,
+  ARuntimeBase,
+  ARuntimeMain,
+  ASystemBase,
+  ASystemEvents,
+  ASystemMain,
+  ASystemResourceString;
+
+// --- Module ---
+
+function ASystemMod_Boot(): AError; stdcall;
+
+function ASystemMod_Fin(): AError; stdcall;
+
+function ASystemMod_GetProc(ProcName: AStr): Pointer; stdcall;
+
+function ASystemMod_Init(): AError; stdcall
+
+implementation
+
+const
+  ASystem_Version = $00070500;
+
+const
+  SystemModule: AModule_Type = (
+    Version: ASystem_Version;
+    Uid: ASystem_Uid;
+    Name: ASystem_Name;
+    Description: nil;
+    Init: ASystemMod_Init;
+    Fin: ASystemMod_Fin;
+    GetProc: ASystemMod_GetProc;
+    Procs: nil;
+    );
+
+// --- ASystemMod ---
+
+function ASystemMod_Boot(): AError;
+begin
+  Result := ARuntime_RegisterModule(SystemModule);
+end;
+
+function ASystemMod_Fin(): AError;
+begin
+  Result := ASystem_Fin();
+end;
+
+function ASystemMod_GetProc(ProcName: AStr): Pointer;
+begin
+  if (ProcName = 'ASystem_Fin') then
+    Result := Addr(ASystem_Fin)
+  else if (ProcName = 'ASystem_GetComments') then
+    Result := Addr(ASystem_GetComments)
+  else if (ProcName = 'ASystem_GetCompanyName') then
+    Result := Addr(ASystem_GetCompanyName)
+  else if (ProcName = 'ASystem_GetConfig') then
+    Result := Addr(ASystem_GetConfig)
+  else if (ProcName = 'ASystem_GetConfigDirectoryPath') then
+    Result := Addr(ASystem_GetConfigDirectoryPath)
+  else if (ProcName = 'ASystem_GetCopyright') then
+    Result := Addr(ASystem_GetCopyright)
+  else if (ProcName = 'ASystem_GetDataDirectoryPath') then
+    Result := Addr(ASystem_GetDataDirectoryPath)
+  else if (ProcName = 'ASystem_GetDescription') then
+    Result := Addr(ASystem_GetDescription)
+  else if (ProcName = 'ASystem_GetDirectoryPath') then
+    Result := Addr(ASystem_GetDirectoryPath)
+  else if (ProcName = 'ASystem_GetExeName') then
+    Result := Addr(ASystem_GetExeName)
+  else if (ProcName = 'ASystem_GetExePath') then
+    Result := Addr(ASystem_GetExePath)
+  else if (ProcName = 'ASystem_GetParamStr') then
+    Result := Addr(ASystem_GetParamStr)
+  else if (ProcName = 'ASystem_GetProgramName') then
+    Result := Addr(ASystem_GetProgramName)
+  else if (ProcName = 'ASystem_GetProductVersionStr') then
+    Result := Addr(ASystem_GetProductVersionStr)
+  else if (ProcName = 'ASystem_GetProgramName') then
+    Result := Addr(ASystem_GetProgramName)
+  else if (ProcName = 'ASystem_GetProgramVersionStr') then
+    Result := Addr(ASystem_GetProgramVersionStr)
+  else if (ProcName = 'ASystem_GetResourceString') then
+    Result := Addr(ASystem_GetResourceString)
+  else if (ProcName = 'ASystem_GetTitle') then
+    Result := Addr(ASystem_GetTitle)
+  else if (ProcName = 'ASystem_GetUrl') then
+    Result := Addr(ASystem_GetUrl)
+  else if (ProcName = 'ASystem_Init') then
+    Result := Addr(ASystem_Init)
+  else if (ProcName = 'ASystem_OnAfterRun_Connect') then
+    Result := Addr(ASystem_OnAfterRun_Connect)
+  else if (ProcName = 'ASystem_OnAfterRun_Disconnect') then
+    Result := Addr(ASystem_OnAfterRun_Disconnect)
+  else if (ProcName = 'ASystem_OnBeforeRun_Connect') then
+    Result := Addr(ASystem_OnBeforeRun_Connect)
+  else if (ProcName = 'ASystem_OnBeforeRun_Disconnect') then
+    Result := Addr(ASystem_OnBeforeRun_Disconnect)
+  else if (ProcName = 'ASystem_Prepare') then
+    Result := Addr(ASystem_Prepare)
+  else if (ProcName = 'ASystem_PrepareA') then
+    Result := Addr(ASystem_PrepareA)
+  else if (ProcName = 'ASystem_ProcessMessages') then
+    Result := Addr(ASystem_ProcessMessages)
+  else if (ProcName = 'ASystem_SetConfig') then
+    Result := Addr(ASystem_SetConfig)
+  else if (ProcName = 'ASystem_SetDataDirectoryPath') then
+    Result := Addr(ASystem_SetDataDirectoryPath)
+  else if (ProcName = 'ASystem_SetOnProcessMessages') then
+    Result := Addr(ASystem_SetOnProcessMessages)
+  else if (ProcName = 'ASystem_SetOnShowErrorA') then
+    Result := Addr(ASystem_SetOnShowErrorA)
+  else if (ProcName = 'ASystem_SetOnShowMessageA') then
+    Result := Addr(ASystem_SetOnShowMessageA)
+  else if (ProcName = 'ASystem_ShellExecute') then
+    Result := Addr(ASystem_ShellExecute)
+  else if (ProcName = 'ASystem_ShowError') then
+    Result := Addr(ASystem_ShowError)
+  else if (ProcName = 'ASystem_ShowErrorA') then
+    Result := Addr(ASystem_ShowErrorA)
+  else if (ProcName = 'ASystem_ShowMessage') then
+    Result := Addr(ASystem_ShowMessage)
+  else if (ProcName = 'ASystem_ShowMessageA') then
+    Result := Addr(ASystem_ShowMessageA)
+  else if (ProcName = 'ASystem_ShowMessageEx') then
+    Result := Addr(ASystem_ShowMessageEx)
+  else if (ProcName = 'ASystem_ShowMessageExA') then
+    Result := Addr(ASystem_ShowMessageExA)
+  else
+    Result := nil;
+end;
+
+function ASystemMod_Init(): AError;
+begin
+  Result := ASystem_Init();
+end;
+
+end.
---
+++ Utils/AUtilsMod.pas
@@ -0,0 +1,161 @@
+{**
+@Author Prof1983 <prof1983@ya.ru>
+@Created 30.09.2009
+@LastMod 01.04.2013
+}
+unit AUtilsMod;
+
+interface
+
+uses
+  ABase,
+  ABaseUtils,
+  ARuntimeBase,
+  ARuntimeMain,
+  AStringUtils,
+  AUtilsBase,
+  AUtilsMain;
+
+// --- AUtilsMod ---
+
+function AUtilsMod_Boot(): AError; stdcall;
+
+function AUtilsMod_Fin(): AError; stdcall;
+
+function AUtilsMod_GetProc(ProcName: AStr): Pointer; stdcall;
+
+function AUtilsMod_Init(): AError; stdcall;
+
+function AUtilsMod_Register(): AError; stdcall;
+
+implementation
+
+const
+  AUtils_Version = $00070500;
+
+const
+  Module: AModule_Type = (
+    Version: AUtils_Version;
+    Uid: AUtils_Uid;
+    Name: AUtils_Name;
+    Description: nil;
+    Init: AUtilsMod_Init;
+    Fin: AUtilsMod_Fin;
+    GetProc: AUtilsMod_GetProc;
+    Procs: nil;
+    );
+
+// --- AUtilsMod ---
+
+function AUtilsMod_Boot(): AError;
+begin
+  Result := AUtilsMod_Register();
+end;
+
+function AUtilsMod_Fin(): AError;
+begin
+  Result := AUtils_Fin();
+end;
+
+function AUtilsMod_GetProc(ProcName: AStr): Pointer;
+begin
+  if (ProcName = 'AUtils_ChangeFileExt') then
+    Result := Addr(AUtils_ChangeFileExt)
+  else if (ProcName = 'AUtils_DateToStr') then
+    Result := Addr(AUtils_DateToStr)
+  else if (ProcName = 'AUtils_DeleteFile') then
+    Result := Addr(AUtils_DeleteFile)
+  else if (ProcName = 'AUtils_DirectoryExists') then
+    Result := Addr(AUtils_DirectoryExists)
+  else if (ProcName = 'AUtils_ExpandFileName') then
+    Result := Addr(AUtils_ExpandFileName)
+  else if (ProcName = 'AUtils_ExtractFileExt') then
+    Result := Addr(AUtils_ExtractFileExt)
+  else if (ProcName = 'AUtils_ExtractFileName') then
+    Result := Addr(AUtils_ExtractFileName)
+  else if (ProcName = 'AUtils_ExtractFilePath') then
+    Result := Addr(AUtils_ExtractFilePath)
+  else if (ProcName = 'AUtils_FileExists') then
+    Result := Addr(AUtils_FileExists)
+  else if (ProcName = 'AUtils_Fin') then
+    Result := Addr(AUtils_Fin)
+  else if (ProcName = 'AUtils_FloatToStr') then
+    Result := Addr(AUtils_FloatToStr)
+  else if (ProcName = 'AUtils_FloatToStr2') then
+    Result := Addr(AUtils_FloatToStr2)
+  else if (ProcName = 'AUtils_ForceDirectories') then
+    Result := Addr(AUtils_ForceDirectories)
+  else if (ProcName = 'AUtils_ForceDirectoriesA') then
+    Result := Addr(AUtils_ForceDirectoriesA)
+  else if (ProcName = 'AUtils_FormatFloat') then
+    Result := Addr(AUtils_FormatFloat)
+  else if (ProcName = 'AUtils_FormatInt') then
+    Result := Addr(AUtils_FormatInt)
+  else if (ProcName = 'AUtils_FormatStr') then
+    Result := Addr(AUtils_FormatStr)
+  else if (ProcName = 'AUtils_FormatStrAnsi') then
+    Result := Addr(AUtils_FormatStrAnsi)
+  else if (ProcName = 'AUtils_FormatStrStr') then
+    Result := Addr(AUtils_FormatStrStr)
+  else if (ProcName = 'AUtils_FormatStrStrA') then
+    Result := Addr(AUtils_FormatStrStrA)
+  else if (ProcName = 'AUtils_GetNowDateTime') then
+    Result := Addr(AUtils_GetNowDateTime)
+  else if (ProcName = 'AUtils_Init') then
+    Result := Addr(AUtils_Init)
+  else if (ProcName = 'AUtils_IntToStr') then
+    Result := Addr(AUtils_IntToStr)
+  else if (ProcName = 'AUtils_NormalizeFloat') then
+    Result := Addr(AUtils_NormalizeFloat)
+  else if (ProcName = 'AUtils_NormalizeStr') then
+    Result := Addr(AUtils_NormalizeStr)
+  else if (ProcName = 'AUtils_NormalizeStrSpace') then
+    Result := Addr(AUtils_NormalizeStrSpace)
+  else if (ProcName = 'AUtils_Power') then
+    Result := Addr(AUtils_Power)
+  else if (ProcName = 'AUtils_ReplaceComma') then
+    Result := Addr(AUtils_ReplaceComma)
+  else if (ProcName = 'AUtils_Round2') then
+    Result := Addr(AUtils_Round2)
+  else if (ProcName = 'AUtils_Sleep') then
+    Result := Addr(AUtils_Sleep)
+  else if (ProcName = 'AUtils_StrToDate') then
+    Result := Addr(AUtils_StrToDate)
+  else if (ProcName = 'AUtils_StrToFloat') then
+    Result := Addr(AUtils_StrToFloat)
+  else if (ProcName = 'AUtils_StrToFloatDef') then
+    Result := Addr(AUtils_StrToFloatDef)
+  else if (ProcName = 'AUtils_StrToInt') then
+    Result := Addr(AUtils_StrToInt)
+  else if (ProcName = 'AUtils_StrToIntDef') then
+    Result := Addr(AUtils_StrToIntDef)
+  else if (ProcName = 'AUtils_Trim') then
+    Result := Addr(AUtils_Trim)
+  else if (ProcName = 'AUtils_TryStrToDate') then
+    Result := Addr(AUtils_TryStrToDate)
+  else if (ProcName = 'AUtils_TryStrToFloat32') then
+    Result := Addr(AUtils_TryStrToFloat32)
+  else if (ProcName = 'AUtils_TryStrToFloat64') then
+    Result := Addr(AUtils_TryStrToFloat64)
+  else if (ProcName = 'AUtils_TryStrToInt') then
+    Result := Addr(AUtils_TryStrToInt)
+  else if (ProcName = 'AString_ToLower') then
+    Result := Addr(AString_ToLower)
+  else if (ProcName = 'AString_ToUpper') then
+    Result := Addr(AString_ToUpper)
+  else
+    Result := nil;
+end;
+
+function AUtilsMod_Init(): AError;
+begin
+  Result := AUtils_Init();
+end;
+
+function AUtilsMod_Register(): AError;
+begin
+  Result := ARuntime_RegisterModule(Module);
+end;
+
+end.
+

5897514210dd61a0be4f3b5434c30e9a64b96361
Prof1983 <prof1983@yandex.ru>
2013-04-12 15:24:57 +0600

    Updated AUiErrorDialog

--- Ui/AUiErrorDialog.pas
+++ Ui/AUiErrorDialog.pas
@@ -1,7 +1,7 @@
 {**
 @Author Prof1983 <prof1983@ya.ru>
 @Created 29.01.2013
-@LastMod 29.01.2013
+@LastMod 12.04.2013
 }
 unit AUiErrorDialog;
 
@@ -39,8 +39,6 @@ type
   public
     Window: AWindow;
     Memo: AControl;
-    Button1: AButton;
-    Button2: AButton;
   public
     DetailStr: string;
     MessageStr: string;
@@ -51,13 +49,11 @@ var
 
 // --- Events ---
 
-function DoErrorWinButtonClick(Obj, Data: AInt): AError; stdcall;
+function DoDetailButtonClick(Obj, Data: AInt): AError; stdcall;
 begin
-  if (ErrorWin.Button1 = Obj) then
-    AUiControl_SetTextP(ErrorWin.Memo, ErrorWin.MessageStr)
-  else if (ErrorWin.Button2 = Obj) then
-    AUiControl_SetTextP(ErrorWin.Memo, ErrorWin.DetailStr);
-  Result := 0;
+  Result := AUiControl_SetTextP(ErrorWin.Memo, ErrorWin.MessageStr + #13#10#13#10 + ErrorWin.DetailStr);
+  AUiTextView_SetScrollBars(ErrorWin.Memo, AUiScrollStyle_Vertical);
+  AUiControl_SetHeight(ErrorWin.Window, 200);
 end;
 
 // --- Private ---
@@ -70,25 +66,13 @@ begin
   ErrorWin.Window := Window;
   AUiControl_SetClientSize(Window, 380, 100);
   AUiControl_SetPosition(Window, 200, 100);
-  AUiControl_SetTextP(Window, 'ВНИМАНИЕ!');
   AUiWindow_SetPosition(Window, AUiWindowPosition_ScreenCenter);
-
-  ErrorWin.Button1 := AUiButton_New(Window);
-  AUiControl_SetPosition(ErrorWin.Button1, 176, 63);
-  AUiControl_SetSize(ErrorWin.Button1, 100, 30);
-  AUiControl_SetTextP(ErrorWin.Button1, 'Кратко');
-  AUiControl_SetOnClick(ErrorWin.Button1, DoErrorWinButtonClick);
-
-  ErrorWin.Button2 := AUiButton_New(Window);
-  AUiControl_SetPosition(ErrorWin.Button2, 272, 63);
-  AUiControl_SetSize(ErrorWin.Button2, 100, 30);
-  AUiControl_SetTextP(ErrorWin.Button2, 'Подробно');
-  AUiControl_SetOnClick(ErrorWin.Button2, DoErrorWinButtonClick);
 
   ErrorWin.Memo := AUiTextView_New(Window, 0);
   AUiControl_SetPosition(ErrorWin.Memo, 1, 6);
   AUiControl_SetSize(ErrorWin.Memo, 370, 50);
   AUiControl_SetColor(ErrorWin.Memo, clBtnFace);
+  AUiControl_SetAlign(ErrorWin.Memo, AUiAlign_Client);
 
   Result := ErrorWin;
 end;
@@ -99,15 +83,18 @@ var
   Window: AWindow;
 begin
   Dialog := AUiDialog_New(AMessageBoxFlags_Ok);
+  if (ExceptMessage <> '') then
+    AUiDialog_AddButton1P(Dialog, 100, 100, 'Подробно', DoDetailButtonClick);
   Window := AUiDialog_GetWindow(Dialog);
   ErrorWin := _CreateErrorForm(Window);
   ErrorWin.MessageStr := UserMessage;
   ErrorWin.DetailStr := ExceptMessage;
-  AUiControl_SetTextP(Window, Caption);
+  if (Caption <> '') then
+    AUiControl_SetTextP(Window, Caption)
+  else
+    AUiControl_SetTextP(Window, 'ВНИМАНИЕ!');
   AUiControl_SetTextP(ErrorWin.Memo, UserMessage);
-  if (ExceptMessage = '') then
-    AUiControl_SetEnabled(ErrorWin.Button2, False);
-  AUiWindow_ShowModal(Window);
+  AUiDialog_ShowModal(Dialog);
   AUiDialog_Free(Dialog);
 end;
 

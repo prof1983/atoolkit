4dc5cb656877f69fb2c8d0996a7513472bd3eeef
Prof1983 <prof1983@yandex.ru>
2013-04-16 13:36:56 +0600

    Added ASettings_NewIniSettings(), ASettings_NewRegSettings()

--- Settings/ASettingsIni.pas
+++ Settings/ASettingsIni.pas
@@ -2,29 +2,72 @@
 @Abstract ASettingsIni
 @Author Prof1983 <prof1983@ya.ru>
 @Created 12.11.2012
-@LastMod 16.11.2012
+@LastMod 16.04.2013
 }
 unit ASettingsIni;
 
 interface
 
 uses
-  ABase, AIniSettings;
+  ABase,
+  AIniSettings,
+  AStringMain;
 
-function ASettings_IniConfig_NewP(const FileName: APascalString): AConfig; stdcall;
+// --- ASettings ---
+
+function ASettings_IniConfig_NewP(const FileName: APascalString): AConfig; stdcall; deprecated {$ifdef ADeprText}'Use ASettings_NewIniSettingsP()'{$endif};
+
+function ASettings_NewIniSettings(const FileName: AString_Type): AConfig; stdcall;
+
+function ASettings_NewIniSettingsA(FileName: AStr): AConfig; stdcall;
+
+function ASettings_NewIniSettingsP(const FileName: APascalString): ASettings;
+
+// --- Settings ---
 
 function Settings_IniConfig_New(const FileName: APascalString): AConfig; stdcall; deprecated; // Use ASettings_IniConfig_NewP()
 
 implementation
 
+// --- ASettings ---
+
 function ASettings_IniConfig_NewP(const FileName: APascalString): AConfig;
+begin
+  Result := ASettings_NewIniSettingsP(FileName);
+end;
+
+function ASettings_NewIniSettings(const FileName: AString_Type): AConfig;
+begin
+  try
+    Result := ASettings_NewIniSettingsP(AString_ToP(FileName));
+  except
+    Result := 0;
+  end;
+end;
+
+function ASettings_NewIniSettingsA(FileName: AStr): AConfig;
+begin
+  try
+    Result := ASettings_NewIniSettingsP(AnsiString(FileName));
+  except
+    Result := 0;
+  end;
+end;
+
+function ASettings_NewIniSettingsP(const FileName: APascalString): ASettings;
 var
   S: TIniSettings;
 begin
-  S := TIniSettings.Create;
-  S.OpenIniFile(FileName);
-  Result := Integer(S);
+  try
+    S := TIniSettings.Create();
+    S.OpenIniFile(FileName);
+    Result := Integer(S);
+  except
+    Result := 0;
+  end;
 end;
+
+// --- Settings ---
 
 function Settings_IniConfig_New(const FileName: APascalString): AConfig;
 begin
--- Settings/ASettingsReg.pas
+++ Settings/ASettingsReg.pas
@@ -2,7 +2,7 @@
 @Abstract ASettingsReg
 @Author Prof1983 <prof1983@ya.ru>
 @Created 12.11.2012
-@LastMod 16.11.2012
+@LastMod 16.04.2013
 }
 unit ASettingsReg;
 
@@ -10,48 +10,76 @@ interface
 
 uses
   {$IFDEF MSWINDOWS}Windows, ARegistrySettings,{$ENDIF}
-  ABase;
+  ABase,
+  AStringMain;
 
-function ASettings_RegConfig_NewP(const Prefix: APascalString): AConfig; stdcall;
+// --- ASettings ---
 
-{deprecated}
-function Settings_RegConfig_New(const Prefix: APascalString): AConfig; stdcall; deprecated; // Use ASettings_RegConfig_NewP()
-function Settings_RegConfig_NewA(const Prefix: APascalString; HKEY: Integer): AConfig; stdcall;
+function ASettings_NewRegSettings(const Prefix: AString_Type; Key: AInt): AConfig; {$ifdef AStdCall}stdcall;{$endif}
+
+function ASettings_NewRegSettingsA(Prefix: AStr; Key: AInt): AConfig; {$ifdef AStdCall}stdcall;{$endif}
+
+function ASettings_NewRegSettingsP(const Prefix: APascalString; Key: AInt): AConfig;
+
+function ASettings_RegConfig_NewP(const Prefix: APascalString): AConfig; stdcall; deprecated {$ifdef ADeptText}'Use ASettings_NewRegSettingsP()'{$endif};
+
+// --- Settings ---
+
+function Settings_RegConfig_New(const Prefix: APascalString): AConfig; stdcall; deprecated {$ifdef ADeprText}'Use ASettings_NewRegSettingsP()'{$endif};
+
+function Settings_RegConfig_NewA(const Prefix: APascalString; HKEY: Integer): AConfig; stdcall; deprecated {$ifdef ADeprText}'Use ASettings_NewRegSettingsP()'{$endif};
 
 implementation
 
-function ASettings_RegConfig_NewP(const Prefix: APascalString): AConfig;
+// --- ASettings ---
+
+function ASettings_NewRegSettings(const Prefix: AString_Type; Key: AInt): AConfig;
+begin
+  Result := ASettings_NewRegSettingsP(AString_ToP(Prefix), Key);
+end;
+
+function ASettings_NewRegSettingsA(Prefix: AStr; Key: AInt): AConfig;
+begin
+  Result := ASettings_NewRegSettingsP(AnsiString(Prefix), Key);
+end;
+
+function ASettings_NewRegSettingsP(const Prefix: APascalString; Key: AInt): AConfig;
+{$ifdef MSWINDOWS}
+var
+  S: TARegistrySettings;
+{$endif}
 begin
   try
-    {$IFDEF MSWINDOWS}
-    Result := Settings_RegConfig_NewA(Prefix, Integer(HKEY_CURRENT_USER));
-    {$ELSE}
+    {$ifdef MSWINDOWS}
+    if (Key = 0) then
+      Key := AInt(HKEY_CURRENT_USER);
+    S := TARegistrySettings.Create;
+    S.Registry.RootKey := Key;
+    S.Prefix := Prefix;
+    Result := ASettings(S);
+    {$else}
     Result := 0;
-    {$ENDIF}
+    {$endif}
   except
     Result := 0;
   end;
 end;
 
+function ASettings_RegConfig_NewP(const Prefix: APascalString): AConfig;
+begin
+  Result := ASettings_NewRegSettingsP(Prefix, 0);
+end;
+
+// --- Settings ---
+
 function Settings_RegConfig_New(const Prefix: APascalString): AConfig;
 begin
-  Result := ASettings_RegConfig_NewP(Prefix);
+  Result := ASettings_NewRegSettingsP(Prefix, 0);
 end;
 
 function Settings_RegConfig_NewA(const Prefix: APascalString; HKEY: Integer): AConfig;
-{$IFDEF MSWINDOWS}
-var
-  S: TARegistrySettings;
-{$ENDIF}
 begin
-  {$IFDEF MSWINDOWS}
-  S := TARegistrySettings.Create;
-  S.Registry.RootKey := HKEY;
-  S.Prefix := Prefix;
-  Result := AConfig(S);
-  {$ELSE}
-  Result := 0;
-  {$ENDIF}
+  Result := ASettings_NewRegSettingsP(Prefix, HKEY);
 end;
 
 end.

dd6a7d5a95eff5ae139cd390cd5194338b86dfaa
Prof1983 <prof1983@yandex.ru>
2013-04-23 11:33:20 +0600

    Added AStringsBoot, AUiBoot

---
+++ Strings/AStringsBoot.pas
@@ -0,0 +1,26 @@
+{**
+@Author Prof1983 <prof1983@ya.ru>
+@Created 23.04.2013
+@LastMod 23.04.2013
+}
+unit AStringsBoot;
+
+interface
+
+uses
+  ABase;
+
+// --- AStrings ---
+
+function AStrings_Boot(): AError;
+
+implementation
+
+// --- AStrings ---
+
+function AStrings_Boot(): AError;
+begin
+  Result := 0;
+end;
+
+end.
---
+++ Ui/AUiBoot.pas
@@ -0,0 +1,26 @@
+{**
+@Author Prof1983 <prof1983@ya.ru>
+@Created 23.04.2013
+@LastMod 23.04.2013
+}
+unit AUiBoot;
+
+interface
+
+uses
+  ABase;
+
+// --- AUi ---
+
+function AUi_Boot(): AError;
+
+implementation
+
+// --- AUi ---
+
+function AUi_Boot(): AError;
+begin
+  Result := 0;
+end;
+
+end.

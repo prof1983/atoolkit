dbf7af52a883f3b53e814342f5811eddceba4e1c
Prof1983 <prof1983@yandex.ru>
2013-04-25 15:01:42 +0600

    Updated APluginsExp

--- Plugins/APluginsExp.pas
+++ Plugins/APluginsExp.pas
@@ -2,7 +2,6 @@
 @Abstract APlugins export functions
 @Author Prof1983 <prof1983@ya.ru>
 @Created 02.08.2011
-@LastMod 27.08.2012
 }
 unit APluginsExp;
 
@@ -12,13 +11,11 @@ uses
   APluginsMain;
 
 exports
-  //APlugins_AddPlugin name 'APlugins_AddPlugin',
-  APlugins_AddPluginA name 'APlugins_AddPluginA',
-  APlugins_Clear name 'APlugins_Clear',
-  APlugins_GetCount name 'APlugins_GetCount',
-  APlugins_Delete name 'APlugins_Delete',
-  //APlugins.Find02 name 'APlugins_Find',
-  APlugins_FindA name 'APlugins_FindA';
+  APlugins_AddPluginA,
+  APlugins_Clear,
+  APlugins_GetCount,
+  APlugins_Delete,
+  APlugins_FindA;
 
 implementation
 

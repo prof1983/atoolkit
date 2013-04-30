ddd47341a4152d96fd5008168a14496681ab886d
Prof1983 <prof1983@yandex.ru>
2013-04-30 15:10:58 +0600

    Updated

--- Base/ASettingsBase.pas
+++ Base/ASettingsBase.pas
@@ -2,16 +2,17 @@
 @Abstract ASettings
 @Author Prof1983 <prof1983@ya.ru>
 @Created 26.07.2012
-@LastMod 26.07.2012
 }
 unit ASettingsBase;
 
 interface
 
 const
-  ASettings_Name = 'ASettings';
-  ASettings_Uid = $10072301;
-
+  ASettings_Name02 = 'Settings';
+  ASettings_Name03 = 'ASettings';
+  ASettings_Name = {$ifdef A02}ASettings_Name02{$else}ASettings_Name03{$endif};
+  ASettings_Uid = $10072301;
+
 implementation
 
 end.
--- Base/ASystemBase.pas
+++ Base/ASystemBase.pas
@@ -2,7 +2,6 @@
 @Abstract ASystem base
 @Author Prof1983 <prof1983@ya.ru>
 @Created 24.10.2011
-@LastMod 26.07.2012
 }
 unit ASystemBase;
 
@@ -12,7 +11,9 @@ uses
   ABase;
 
 const
-  ASystem_Name = 'ASystem';
+  ASystem_Name02 = 'System';
+  ASystem_Name03 = 'ASystem';
+  ASystem_Name = {$ifdef A02}ASystem_Name02{$else}ASystem_Name03{$endif};
   ASystem_Uid = $07082001;
 
 type
